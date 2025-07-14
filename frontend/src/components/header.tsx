3dfe0a6024a836a64d5580b8721b46ca1ad19ad8
jtayped <jtayped@gmail.com>
2025-07-14 16:53:27 +0200

    removed time badge from header

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Badge, Col, Row } from "react-bootstrap";
+import { Col, Row } from "react-bootstrap";
 
 const UserHeader = () => {
   return (
@@ -7,9 +7,6 @@ const UserHeader = () => {
       <Col>
         <div className="d-flex justify-content-between align-items-center">
           <h1 className="h3 mb-0">User Panel</h1>
-          <Badge bg="primary" className="fs-6">
-            12:12:12
-          </Badge>
         </div>
       </Col>
     </Row>
